6dbce2951fb66395ea2457497dff6e62f88b60bb
abdulazizgh <abdulaziz.binghannam@gmail.com>
2020-09-14 01:40:42 +0300

    Docker, Kubernetes, Linter setup

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="abdulazizgh/ml-microservices"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservices --image=$dockerpath --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl wait --for=condition=ready pod --all
+kubectl port-forward ml-microservices 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="abdulazizgh/ml-microservices"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml-microservices $dockerpath
+docker login --username abdulazizgh
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
